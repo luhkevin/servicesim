3a19c5015a8f2873d4c8c780c57a966d229d0f7d
Kevin Lu <luhkevin@gmail.com>
2016-03-16 17:19:58 -0700

    simclient

--- simclient.py
+++ simclient.py
@@ -1,12 +1,39 @@
+import time
 import requests
 
-# We assume that there is a controller node running on localhost:5000
-def run():
-    # Send a SETUP message to each node
-    requests.get('http://localhost:5000/controlller')
+def run(address, port, latency):
+    # First, sleep so the controller node has time to spin up
+    time.sleep(3)
 
-    # Start the clients
+    # Send a SETUP message to each node
+    controller_url = 'http://' + str(address) + ':' + str(port)
+    requests.post(url + '/controller/all')
+
+    # Sleep for the other nodes' to accept their configuration
+    time.sleep(3)
+
+    # Setup some stats
+    requests.post(url + '/setstatus/gamma/404')
+    requests.post(url + '/setstatus/kappa/404')
+
+    # Start the client
+    while True:
+        time.sleep(float(latency))
+        requests.post(url + '/start/all')
 
 
 if __name__=='__main__':
     run()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--address', default='localhost', help="Specify the address of the controller")
+    parser.add_argument('-p', '--port', , default='8080', help="Specify the port of the controller")
+    parser.add_argument('-l', '--latency', , default='0.1', help="Specify the latency of requests")
+
+    args = parser.parse_args()
+
+    run(args.address, args.port, args.latency)
+
