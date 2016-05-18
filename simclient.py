a248b60a91adbfe8558fd7ed71575f0504438824
Kevin Lu <kevin@netsil.com>
2016-05-18 23:07:51 +0000

    simclient loop

--- simclient.py
+++ simclient.py
@@ -4,6 +4,8 @@ import argparse
 import json
 
 def parse_client_nodes(sim_config_path):
+    """For now, we will only use the first node in the cluster to initiate the requests
+    """
     client_urls = list()
     with open(sim_config_path, 'r') as sim_config:
         config = json.load(sim_config)
@@ -14,17 +16,17 @@ def parse_client_nodes(sim_config_path):
             if node_id in client_node_ids:
                 for uri in node['uris']:
                     port = node['port']
-                    client_urls.append('dev-' + node_id + '.marathon.mesos' + ':' + str(port) + uri)
+                    client_urls.append('http://' + 'dev-' + node_id + '-0' + '.marathon.mesos' + ':' + str(port) + uri)
     return client_urls
 
 def run(latency, config):
     client_urls = parse_client_nodes(config)
     print client_urls
     # Start the client
-    #while True:
-    #    for url in client_urls:
-    #        time.sleep(float(latency))
-    #        requests.post(url + '/start/all')
+    while True:
+        for url in client_urls:
+            time.sleep(float(latency))
+            requests.get(url)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
